08acb5a52b72d56004f930d7572a9385158710a3
pionnertch <elaion256@outlook.com>
2015-02-22 18:09:56 -0300

    pleasant

--- grapher.php
+++ grapher.php
@@ -1,6 +1,17 @@
 <?php
 
-include_once "http://eque-e.cl/backend/specialcharacter.php";
+
+renderPhpToString("http://eque-e.cl/backend/specialcharacter.php");
+
+function renderPhpToString($file, $vars=null)
+{
+    if (is_array($vars) && !empty($vars)) {
+        extract($vars);
+    }
+    ob_start();
+    include $file;
+    return ob_get_clean();
+}
 
 
 ?>
